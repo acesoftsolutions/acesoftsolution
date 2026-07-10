f0d22240f2dbe601dd57dd0f94c90095e785e33a
chiragparihar7 <chiragparihar118@gmail.com>
2026-07-10 13:54:05 +0530

    feat: add Microsoft Clarity tracking script and Bing site authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
+    process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   ),
 
   title: {
@@ -60,10 +60,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="en"
-      className={`${poppins.variable} ${inter.variable}`}
-    >
+    <html lang="en" className={`${poppins.variable} ${inter.variable}`}>
       <head>
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
@@ -98,6 +95,20 @@ export default function RootLayout({
 
             gtag('config', 'G-JGG1E3861H');
           `}
+        </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);
+      t.async=1;
+      t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xk67cz5e0e");
+  `}
         </Script>
       </head>
 
